02edb538fbfc390dd5e7dac1347bbe8c41613872
gabrielvc <gvictorinocardoso@gmail.com>
2017-02-23 21:13:28 +0100

    Update recueil_simule.py

--- recueil_simule.py
+++ recueil_simule.py
@@ -28,6 +28,22 @@ def value(M,R,L,lat_dbs):
     return val
 
 
+def get_voisins(graph, M):
+    crit = .5
+    voisins = []
+    div = len(M)
+    while(len(voisins)==0):
+        for q in graph:
+            cur = np.multiply(q.state,M)
+            val_q = np.multiply(q.state,q.state)
+            val_M = np.multiply(M,M)
+            cur = np.sum(cur,axis=None)/np.min([val_M, val_q])
+
+            if cur>crit:
+                voisins.append(q)
+        crit = crit-.1
+    return voisins
+
 
 def create_M(S,X, size):
     M = np.zeros(size)
@@ -55,11 +71,37 @@ def create_graph(S,X,R,L,N_cache, N_films, lat_dbs,qt):
         val = score(M,L,R,N_cache,lat_dbs)
 
         graph.append(node(M,val))
-        graph[-1].linkneighbours(graph[:len(graph)-1])
+        if len(graph)>1:
+            graph[-1].linkneighbours(get_voisins(graph[:len(graph)-1],M))
+        if j%100==0:
+            print(j)
+
     return graph
 
+def line_ok(line):
+    return sum(int(S[i]) for i in range(len(line)) if line[i] == 1) <= X
 
-graph = create_graph(S,X,requests,cacheLatencies,C,V,Ld,30000)
+def create_M_2(n_pop):
+        
+    # pour chaque cache server
+    M = np.zeros((C,V))
+
+    for i in range(C):
+        shuffled_videos = np.arange(V)
+        np.random.shuffle(shuffled_videos)
+        for v in shuffled_videos:
+            tmp = M[i].copy()
+            tmp[v] = 1
+            if line_ok(tmp):
+                M[i,v] = 1
+
+
+    return M
+
+
+
+
+graph = create_graph(S,X,requests,cacheLatencies,C,V,Ld,3000)
 resultat = graph[0].hasting(20000,1000000,.999)
 
 print(resultat.value)
